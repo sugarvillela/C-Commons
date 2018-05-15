561b5467887c92ae0be7bfc4f90078cd4330f128
Dave Swanson <davesswanson@hotmail.com>
2018-05-15 01:02:20 -0700

    Initial Commit

---
+++ qStack.cpp
@@ -0,0 +1,68 @@
+#include "../header/qStack.h"
+void test_qstack_stack(){
+    cout << "Test stack: Pushing five! four three two one zero" << endl;
+    QStack< string > Q;
+    Q.push("five!");
+    Q.push("four ");
+    Q.push("three ");
+    Q.push("two ");
+    Q.push("one ");
+    Q.push("zero ");
+    cout << "Top element is: " << Q.top() << endl;
+    cout << "Start popping:" << endl;
+    while( !Q.empty() ){
+        cout << Q.pop();
+    }
+    cout << endl << endl;
+}
+void test_qstack_itr(){
+    cout << "Test itr: Pushing five! four three two one zero" << endl;
+    QStack< string > Q;
+    Q.push("five!");
+    Q.push("four ");
+    Q.push("three ");
+    Q.push("two ");
+    Q.push("one ");
+    Q.push("zero ");
+    cout << "Start iterating:" << endl;
+    Q.reset();          //set 'next' pointer to top element
+    string returnMe=""; //for return value; Q.next returns 0 when done
+    while( Q.next( returnMe ) ){
+        cout << returnMe;
+    }
+    cout << endl << endl;
+}
+void test_qstack_at(){
+    cout << "Test at() function: Pushing five! four three two one zero" << endl;
+    QStack< string > Q;
+    Q.push("five!");
+    Q.push("four ");
+    Q.push("three ");
+    Q.push("two ");
+    Q.push("one ");
+    Q.push("zero ");
+    cout << "Using at() 0 through 5:" << endl;
+    int i=0;
+    string returnMe=""; //for return value; Q.at returns 0 when out of range
+    while( Q.at( i, returnMe ) ){
+        cout << i << ":" << returnMe << endl;
+        i++;
+    }
+    cout << endl << endl;
+}
+void test_qstack_queue(){
+    cout << "Test queue functions: Pushing five! four three two one zero" << endl;
+    QStack< string > Q;
+    Q.enqueue("five!");
+    Q.enqueue("four ");
+    Q.enqueue("three ");
+    Q.enqueue("two ");
+    Q.enqueue("one ");
+    Q.enqueue("zero ");
+    cout << "Bottom element is: " << Q.bottom() << endl;
+    cout << "Start dequeueing:" << endl;
+    while( !Q.empty() ){
+        cout << Q.dequeue();
+    }
+    cout << endl << endl;
+}
---
+++ qStack.h
@@ -0,0 +1,190 @@
+/* Template function that can be a stack, a queue or an iterator.
+ * QStack is implemented on a linked list data structure.
+ * For stack operations, call push, pop, top, size and empty.
+ * For queue operations, call enqueue, dequeue, bottom, size and empty. 
+ * For iterator operations, call reset, next, and at.
+ * 'Reset' sets pointer to top and 'next' gets an item and increments pointer
+ * 'At' pretends there is an index and searches to that point.
+ * 'Next' and 'at' return boolean. The return value is a 'reference' parameter.
+ * 'FlipCopy' returns the same list backwards.
+ * 'Disp' works as long as T is printable, a string or number
+ * All functions are demonstrated in the test functions in qStack.cpp
+ */
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<class T>
+struct Node{
+	Node(){ }
+    Node(T val) {
+		data = val;
+		next = NULL;
+		prev = NULL;
+	}
+	T data;
+	Node<T>* next;
+	Node<T>* prev;
+};
+
+template<class T>
+class QStack{
+public:
+	QStack();
+	~QStack(void);
+	/* Stack functions */
+	void push(T);
+	T pop();
+	T top();
+	int size();
+	bool empty();
+	/* Queue functions */
+	void enqueue( T value );
+	T dequeue();
+	T bottom();
+	/* Iterative functions */
+	void reset();
+	int itrDone();
+	int next( T &returnMe );
+	int at( int index, T &returnMe );
+	/* Misc functions */
+	void flipCopy(QStack &nu);
+	void disp(string label);
+	
+private:
+	int length;
+	Node<T>* sTop;//stack top
+	Node<T>* sBot;//stack bottom = dequeue point
+	Node<T>* curr;//iterator pointer
+};
+
+template<class T>
+QStack<T>::QStack(){
+	sTop = sBot = curr = NULL;
+	length = 0;
+}
+template<class T>
+QStack<T>::~QStack(){
+	for (Node<T>* victim=sTop; victim != NULL; victim=sTop){
+		sTop = sTop->next;
+		delete victim;
+	}
+}
+
+template<class T>
+void QStack<T>::push(T value){//stack push or queue enqueue
+	Node<T>* temp = new Node<T>(value);
+	temp->data = value;
+	temp->next = sTop;
+	if(length == 0){//bottom for dequeue
+	    sBot = temp;
+	}
+	else{
+	    sTop->prev=temp;
+	}
+	sTop = temp;    //top for stack pop
+	length++;
+}
+
+template<class T>
+T QStack<T>::pop(){//returns a value and deletes top
+    if(!sTop){ return NULL; }
+	T ret = sTop->data;
+	Node<T>* victim = sTop;
+	sTop = victim->next;
+	victim->next = NULL;
+	delete victim;
+	length--;
+	if(!length){
+	    sBot=NULL;
+	}
+	return ret;
+}
+template<class T>
+T QStack<T>::top(){//returns a value and leaves top
+	return (sTop)? sTop->data : NULL;
+}
+template<class T>
+int QStack<T>::size(){
+	return length;
+}
+template<class T>
+bool QStack<T>::empty(){
+	return (length == 0);
+}
+/* Queue functions */
+template<class T>
+void  QStack<T>::enqueue( T value ){//same as push
+    push( value );
+}
+template<class T>
+T  QStack<T>::dequeue(){//Return a value and delete bottom
+    if(!sBot){ return NULL; }
+	T ret = sBot->data;
+	Node<T>* victim = sBot;
+	sBot = victim->prev;
+	victim->prev = NULL;
+	delete victim;
+	length--;
+	if(!length){
+	    sTop=NULL;
+	}
+	return ret;
+}
+template<class T>
+T  QStack<T>::bottom(){//Return a value and keep bottom
+    return (sBot)? sBot->data : NULL;
+}
+/* Iterative functions: iterator works from stack perspective: top to bottome */
+template<class T>
+void QStack<T>::reset(){
+    curr=sTop;
+}
+template<class T>
+int QStack<T>::itrDone(){//increments on next() so len-1 is done
+    return ( curr==NULL || curr->next==NULL );
+}
+template<class T>
+int QStack<T>::next( T &returnMe ){//O(1) because it grabs next
+    if( ( curr=curr->next )!=NULL ){
+        returnMe=curr->data;
+        return 1;
+    }
+    return 0;
+}
+template<class T>
+int QStack<T>::at( int index, T &returnMe ){//O(n) because it traverses
+    int ci=0;
+	for (Node<T>* i=sTop; i!=NULL; i=i->next ){
+	    if( index == ci++ ){
+	        returnMe=i->data;
+	        return 1;
+	    }
+	}
+	return 0;
+}
+/* Misc functions */
+template<class T>
+void QStack<T>::disp(string label){
+	cout << endl << label << endl;
+	for (Node<T>* i=sTop; i!=NULL; i=i->next){
+		cout << " (" << i->data << ") ";
+	}
+	cout << endl;
+}
+template<class T>
+void QStack<T>::flipCopy(QStack &nu){//copies or replaces in reverse order
+	while(!nu.empty())
+	{
+		nu.pop();
+	}
+	for (Node<T>* i=sTop; i!=NULL; i=i->next){
+		nu.push(i->data);
+	}
+}
+
+/* Test functions in qStack.cpp */
+extern void test_qstack_stack();
+extern void test_qstack_itr();
+extern void test_qstack_at();
+extern void test_qstack_queue();
